b778c576c6af331085c8012322fe4ff8e0077b67
vuki-dev <vuki.pavlovic@yahoo.com>
2024-04-03 15:42:24 +0200

    added theme toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import ThemeSwitch from "@/components/theme-switch";
 
 export default function Home() {
   return (
     <div>
+      <ThemeSwitch />
       <h1>Hello discord clone</h1>
     </div>
   );

9e38e713fbb988218b50354eb99673f9917b115a
vuki-dev <vuki.pavlovic@yahoo.com>
2024-04-03 14:16:10 +0200

    dark mode added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Open_Sans } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const OpenSans = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +16,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={OpenSans.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          storageKey="discord-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }

83be8856785706d0f9835742cc522a2bed1ca379
vuki-dev <vuki.pavlovic@yahoo.com>
2024-04-06 21:34:07 +0200

    setting up jwt auth

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,21 +1,24 @@
-import db from "@/lib/db";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { registerFormSchema } from "@/lib/validation";
+import { registerProfile, getProfileId } from "@/lib/db-querys";
+import { createToken } from "@/lib/auth";
 
 export async function POST(req: Request) {
-    try{
-        const {username, email, password} = await req.json();
-        const query = `INSERT INTO profile (name, email, password) VALUES (?, ?, ?)`
-        db.query(query, [username, email, password], (err, result) => {
-            if (err) {
-                throw new Error(err.message)
-            } else {
-                console.log('Data inserted into MySQL successfully');
-            }
-        });
+  try {
+    const { username, email, password } = await registerFormSchema.parseAsync(
+      await req.json()
+    )
 
-        return new NextResponse("Registered successfully", { status: 200 });
-    } catch(error) {
-        console.log('[REGISTER_POST]', error);
-        return new NextResponse("Internal Error", {status: 500})
-    }
+    const hashedPassword = await bcrypt.hash(password, 5);
+
+    await registerProfile(username, email, hashedPassword);
+    const profileId = await getProfileId(email);
+    console.log(profileId);
+
+    return new NextResponse("Registered successfully", { status: 200 });
+  } catch (error) {
+    console.log("[REGISTER_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
---
+++ src/lib/db-querys.ts
@@ -0,0 +1,34 @@
+import db from "./db";
+
+const registerProfile = async (username: string, email: string, hashedPassword: string) => {
+    const query = `INSERT INTO profile (name, email, password) VALUES (?, ?, ?)`;
+
+    await new Promise ((res, rej) => {
+        db.query(query, [username, email, hashedPassword], (err, result) => {
+          if (err) {
+            rej(err.message)
+          } else {
+            console.log("Data inserted into MySQL successfully");
+            res(result);
+          }
+        });
+    });
+}
+
+const getProfileId = async (email: string) => {
+    let selectQuery = `SELECT id FROM profile WHERE email = ?`;
+
+    const profileId = await new Promise((res, rej) => {
+        db.query(selectQuery, [email], (err, result) => {
+            if (err) {
+                rej(err.message)
+            } else {
+                res(result[0].id);
+            }
+        })
+    })
+
+    return profileId;
+}
+
+export { registerProfile, getProfileId };

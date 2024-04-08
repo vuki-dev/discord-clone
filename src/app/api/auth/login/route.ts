7f4cfe183d477500f9faf0ce51fa8c6a04dfa998
vuki-dev <vuki.pavlovic@yahoo.com>
2024-04-08 12:55:35 +0200

    added login auth - todo: improve error handling ui on front and add additional validations

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,24 @@
+import { createToken } from "@/lib/auth";
+import { userLogin } from "@/lib/db-querys";
+import { loginFormSchema } from "@/lib/validation";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+    try{
+        const {email, password} = await req.json();
+        const user = await userLogin(email, password);
+        const profileId = user.id;
+        const token = await createToken(profileId);
+        
+        return new NextResponse("Logged in successfully", { status: 200, headers: { 'Set-Cookie': `token=${token}; Path=/; Expires=${24*60*60}`} });
+    } catch (err) {
+        if (err instanceof Error) {
+            return new NextResponse(`${err.message}`, { status: 500 });
+          } else {
+            return new NextResponse("Internal server error", { status: 500 });
+        }
+    }
+    
+
+}

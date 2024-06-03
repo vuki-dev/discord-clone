6c9b1edd47b13513a7ca2af196bc825ab1b275ff
vukasin <vukasin.pavlovic345@gmail.com>
2024-06-03 16:09:35 +0200

    added chat scroll hook

--- src/components/chat/chat-messages.tsx
+++ src/components/chat/chat-messages.tsx
@@ -8,6 +8,7 @@ import { Fragment, useRef, ElementRef } from "react";
 import ChatItem from "./chat-item";
 import { format } from "date-fns";
 import { useChatSocket } from "@/hooks/use-chat-socket";
+import { useChatScroll } from "@/hooks/use-chat-scroll";
 
 const DATE_FORMAT = "d MMM yyyy, HH:mm"
 
@@ -49,6 +50,13 @@ const ChatMessages = ({
     })
 
     useChatSocket({queryKey, addKey, updateKey});
+    useChatScroll({
+        chatRef,
+        bottomRef,
+        loadMore: fetchNextPage,
+        shouldLoadMore: !isFetchingNextPage && !!hasNextPage,
+        count: data?.pages?.[0]?.items?.length ?? 0
+    })
 
     if(status === "pending") {
         return (
